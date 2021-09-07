07c5c7c8d4b61e09490d0f3fd09644e7ebaf18bf
spazdrum <spazdrum@gmail.com>
2021-09-07 10:15:34 -0400

    Spazify to Spotify

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 className="main-heading">Spazify Song Finder</h1>;
+  return <h1 className="main-heading">Spotify Song Finder</h1>;
 };
 
 export default Header;
